cc3840961362f2f16c71ec0cc617285d542148c0
Mo <646309547@qq.com>
2017-07-06 16:48:06 +0800

    主页bottom导航完成
    listview切换完成，切换动画利用GitHub开源代码完成

--- ELP/app/src/main/java/zero/zucc/com/elp/MainActivity.java
+++ ELP/app/src/main/java/zero/zucc/com/elp/MainActivity.java
@@ -3,11 +3,7 @@ package zero.zucc.com.elp;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.widget.CardView;
 import android.support.v7.widget.SearchView;
-import android.view.Gravity;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -19,36 +15,38 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.List;
 
+import etong.bottomnavigation.lib.BottomNavigationBar;
 import zero.zucc.com.elp.Adapter.RecommendAdapter;
-import zero.zucc.com.elp.Fragment.Course_LessonFragment;
 import zero.zucc.com.elp.Fragment.Course_ListFragment;
 import zero.zucc.com.elp.Item.Course;
-import zero.zucc.com.elp.Item.User;
-import zero.zucc.com.elp.Tool.AvatarImageView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
     ListView recommend;
     SearchView searchView;
     Toolbar toolbar;
+    BottomNavigationBar bottomNavigationBar;
     ArrayList<Course> listdata = new ArrayList<>();
-
+    RecommendAdapter recommendAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
-
+        bottomNavigationBar = (BottomNavigationBar) findViewById(R.id.bottomLayout);
+        bottomNavigationBar.addTab(R.drawable.history_white, "历史课程", 0xff4a5965);
+        bottomNavigationBar.addTab(R.drawable.recommend_white, "推荐课程", R.color.accent);
+        bottomNavigationBar.addTab(R.drawable.all_white, "所有课程", R.color.colorPrimary);
         setSupportActionBar(toolbar);
         //加载数据
-        initData();
+        initDataHS();
 
         recommend = (ListView)findViewById(R.id.recommend) ;
-        RecommendAdapter recommendAdapter = new RecommendAdapter(this,listdata);
+        recommendAdapter = new RecommendAdapter(this,listdata);
         recommend.setAdapter(recommendAdapter);
         recommend.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -64,12 +62,19 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        bottomNavigationBar.setOnTabListener(new BottomNavigationBar.TabListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onSelected(int position) {
+                if(position==0){
+                    initDataHS();
+                    recommendAdapter.notifyDataSetChanged();
+                }else if(position==1){
+                    initDataRM();
+                    recommendAdapter.notifyDataSetChanged();
+                }else{
+                    initDataAL();
+                    recommendAdapter.notifyDataSetChanged();
+                }
             }
         });
 
@@ -83,7 +88,26 @@ public class MainActivity extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
     }
 
-    private void initData(){
+    private void initDataHS(){
+        listdata.clear();
+        Course c = new Course() ;
+        c.setCourseName("JavaScript");
+        c.setCoursePic(R.drawable.js);
+        c.setCourseInfo("JavaScript 是互联网上最流行的脚本语言，这门语言可用于 HTML 和 web，更可广泛用于服务器、PC、笔记本电脑、平板电脑和智能手机等设备。");
+        listdata.add(c);
+    }
+
+    private void initDataRM(){
+        listdata.clear();
+        Course c = new Course() ;
+        c.setCourseName("Struts2框架");
+        c.setCoursePic(R.drawable.struts);
+        c.setCourseInfo("Struts2以WebWork优秀的设计思想为核心，吸收了 Struts框架的部分优点，提供了一个更加整洁的MVC设计模式实现的Web 应用程序框架");
+        listdata.add(c);
+    }
+
+    private void initDataAL(){
+        listdata.clear();
         Course c = new Course() ;
         c.setCourseName("JavaScript");
         c.setCoursePic(R.drawable.js);
@@ -95,6 +119,7 @@ public class MainActivity extends AppCompatActivity
         c.setCourseInfo("Struts2以WebWork优秀的设计思想为核心，吸收了 Struts框架的部分优点，提供了一个更加整洁的MVC设计模式实现的Web 应用程序框架");
         listdata.add(c);
     }
+
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
